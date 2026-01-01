10a3f16de187e6effc0c694055588707cd5488ef
jimbeamdiablo <mikejackson674@gmail.com>
2026-01-01 13:20:54 -0600

    Add files via upload

--- Contents/mods/AutoLoggingB42/42.13/media/lua/client/jb_ClearingLogic.lua
+++ Contents/mods/AutoLoggingB42/42.13/media/lua/client/jb_ClearingLogic.lua
@@ -1,37 +1,54 @@
 -- jb_ClearingLogic.lua
 JBLogging = JBLogging or {}
-local ActionSpeedKeeper = require("jb_SpeedKeeper")
-
--- need a timed action player for the server side of things
--- so we don't stuff 100 actions in to the queue at once
+require("jb_ActionPlayer")
 
 JBLogging.unifiedClear = function(playerObj, worldObjs, selectedArea, clearType)
-    if not selectedArea then return end
-    
-    local actionSpeedKeeper = ActionSpeedKeeper:new(playerObj)
-    
+    if not selectedArea or not selectedArea.squares then return end
+
+    -- loop selected area for targets
     for _, square in ipairs(selectedArea.squares) do
         if clearType == "Tree" and square:HasTree() then
-            ISWorldObjectContextMenu.doChopTree(playerObj, square:getTree())
-
+            -- queue it
+            JBLogging.ActionPlayer.addToQueue(
+                playerObj,
+                ISWorldObjectContextMenu.doChopTree,
+                { playerObj, square:getTree() }
+            )
         elseif clearType == "Grass" then
+            local hasGrass = false
             for i = 1, square:getObjects():size() do
                 local o = square:getObjects():get(i - 1)
                 if o:getProperties() and o:getProperties():has(IsoFlagType.canBeRemoved) then
-                    ISWorldObjectContextMenu.doRemoveGrass(playerObj, square)
+                    hasGrass = true
+                    break
                 end
             end
 
+            if hasGrass then
+                JBLogging.ActionPlayer.addToQueue(
+                    playerObj,
+                    ISWorldObjectContextMenu.doRemoveGrass,
+                    { playerObj, square }
+                )
+            end
         elseif clearType == "Bush" then
+            -- Check for bushes
+            local hasBush = false
             for i = 1, square:getObjects():size() do
                 local o = square:getObjects():get(i - 1)
                 if o:getSprite() and o:getSprite():getProperties() and o:getSprite():getProperties():has(IsoFlagType.canBeCut) then
-                    ISWorldObjectContextMenu.doRemovePlant(playerObj, square, false)
+                    hasBush = true
+                    break
                 end
             end
 
+            if hasBush then
+                JBLogging.ActionPlayer.addToQueue(
+                    playerObj,
+                    ISWorldObjectContextMenu.doRemovePlant,
+                    { playerObj, square, false }
+                )
+            end
         end
     end
-    
-    actionSpeedKeeper:KeepSpeed()
 end
