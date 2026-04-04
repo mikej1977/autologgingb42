3fc4352981e7f8c28afc7b82f83e3adb8b2c489e
jimbeamdiablo <mikejackson674@gmail.com>
2026-04-04 15:54:16 -0500

    Add files via upload

---
+++ 42.16/media/lua/shared/helpers/jb_Predicates.lua
@@ -0,0 +1,15 @@
+-- jb_Predicates.lua
+
+JBLogging = JBLogging or {}
+
+JBLogging.Predicates = {
+    ChopTree = function(item) return not item:isBroken() and item:hasTag("CHOP_TREE") end,
+    WoodSaw = function(item) return not item:isBroken() and item:hasTag("SAW") end,
+    CutPlant = function(item) return not item:isBroken() and item:hasTag("CUT_PLANT") end,
+    DigStump = function(item) return not item:isBroken() and item:hasTag("REMOVE_STUMP") end,
+    Digging = function(item)
+        if item:isBroken() then return false end
+        return item:hasTag("HAMMER") or item:hasTag("SLEDGEHAMMER") or item:hasTag("CLUB_HAMMER") or
+            item:hasTag("PICK_AXE") or item:getType() == "PickAxe" or item:hasTag("STONE_MAUL")
+    end,
+}
